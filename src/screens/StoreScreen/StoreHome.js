2b49100b032f82b937021ade5883e0a1a25015be
yeyeyeeeejin <79955539+yeyeyeeeejin@users.noreply.github.com>
2022-06-10 15:42:44 +0900

    design commit

--- src/screens/StoreScreen/StoreHome.js
+++ src/screens/StoreScreen/StoreHome.js
@@ -106,29 +106,10 @@ const StoreHome = ({navigation}) => {
         activeOpacity={0.8}
         onPress={() => navigation.navigate('Details', plant)}>
         <View style={style.card}>
-          <View style={{alignItems: 'flex-end'}}>
-            <View
-              style={{
-                width: 30,
-                height: 30,
-                borderRadius: 20,
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: //plant.like
-                  //? 'rgba(245, 42, 42,0.2)': 
-                  'rgba(0,0,0,0.2) ',
-              }}>
-              <Icon
-                name="favorite"
-                size={18}
-                //color={plant.like ? COLORS.red : COLORS.black}
-              />
-            </View>
-          </View>
-
           <View
             style={{
               height: 90,
+              marginTop:15,
               alignItems: 'center',
             }}>
             <Image
