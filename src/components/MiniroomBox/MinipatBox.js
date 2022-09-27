5712fb08e6b3b0f96bf9adaf3c899ac951745fab
sihun <thrchtlgns@naver.com>
2022-09-27 15:40:52 +0900

    [feat] 미니미 오류 수정

--- src/components/MiniroomBox/MinipatBox.js
+++ src/components/MiniroomBox/MinipatBox.js
@@ -1,56 +1,66 @@
-import {View ,StyleSheet,Animated,PanResponder,Image,Text} from 'react-native';
-import React,{useRef, useState,useEffect} from 'react'
+import {
+  View,
+  StyleSheet,
+  Animated,
+  PanResponder,
+  Image,
+  Text,
+} from 'react-native';
+import React, {useRef, useState, useEffect} from 'react';
 import useStore from '../../../store/store';
 import firestore from '@react-native-firebase/firestore';
 import firebase from '@react-native-firebase/app';
 import Draggable from 'react-native-draggable';
-import { useSelector } from 'react-redux';
-import { resolvePlugin } from '@babel/core';
+import {useSelector} from 'react-redux';
+import {resolvePlugin} from '@babel/core';
 
-const MinipatBox =({}) => {
-  const [Minipat, setMinipat] = useState()
-  const [Patarr, setPatarr] = useState();
-  const addminipat = firestore().collection('miniroom').doc(firebase.auth().currentUser.uid).collection('room').doc(firebase.auth().currentUser.uid).collection('minipat').doc(firebase.auth().currentUser.uid+'mid');
-  const {countItem,isMinipat} = useStore();
-  const count = useSelector(state => {return state.count.value});
-  
-  const getMinipat = async() => {
+const MinipatBox = ({}) => {
+  const [Minipat, setMinipat] = useState();
+  const addminipat = firestore()
+    .collection('miniroom')
+    .doc(firebase.auth().currentUser.uid)
+    .collection('room')
+    .doc(firebase.auth().currentUser.uid)
+    .collection('minipat')
+    .doc(firebase.auth().currentUser.uid + 'mid');
+  const {countItem, isMinipat} = useStore();
+  const count = useSelector(state => {
+    return state.count.value;
+  });
+
+  const getMinipat = async () => {
     try {
       const datatool = await addminipat.get();
-      setMinipat(datatool._data);      
-      console.log(`시훈테스트 ${Minipat.address1}`)
-      // let strArr = Object.keys(Minipat).map(item => Minipat[item]);
-      // let newstrArr = strArr.sort();
-      // setPatarr(newstrArr.slice(0,6));
-      // console.log(Patarr);
-    } catch (error) { 
+      setMinipat(datatool._data);
+    } catch (error) {
       console.log(error.message);
     }
   };
 
   useEffect(() => {
     getMinipat();
-    return () => {
-    }
-  }, [countItem,isMinipat,count]);
-    return(
-      <View style={{position:'absolute',borderWidth:1,}}>
-        {
-          Minipat ?
-           <Draggable x={320} y={110}z={0} renderSize={80} imageSource={{uri:`${Minipat[count]}`}}shouldReverse={true}></Draggable>
-           :
-           <Text>asdasds</Text>
-        }
-                  
-        </View>
-        )
-    }
+    return () => {};
+  }, [countItem, isMinipat, count]);
+  return (
+    <View style={{position: 'absolute', borderWidth: 1}}>
+      {Minipat && (
+        <Draggable
+          x={320}
+          y={110}
+          z={0}
+          renderSize={80}
+          imageSource={{uri: `${Minipat[count]}`}}
+          shouldReverse={true}></Draggable>
+      )}
+    </View>
+  );
+};
 
-    const styles =StyleSheet.create({
-        box:{
-            height: 70,
-            width: 70,
-            position:'absolute'
-          },
-    });
-    export default MinipatBox
+const styles = StyleSheet.create({
+  box: {
+    height: 70,
+    width: 70,
+    position: 'absolute',
+  },
+});
+export default MinipatBox;
--- src/screens/ProfileScreen/Miniroom/MinimiInven.js
+++ src/screens/ProfileScreen/Miniroom/MinimiInven.js
@@ -1,26 +1,47 @@
-import { View, Text,TouchableOpacity,StyleSheet,Image,SafeAreaView,Button,Dimensions,ScrollView} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  SafeAreaView,
+  Button,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
 import storage from '@react-native-firebase/storage';
 import firestore from '@react-native-firebase/firestore';
-import React,{useState,useEffect} from 'react'
-import firebase  from '@react-native-firebase/app';
+import React, {useState, useEffect} from 'react';
+import firebase from '@react-native-firebase/app';
 import useStore from '../../../../store/store';
-
+import {useNavigation} from '@react-navigation/native';
 const MinimiInven = () => {
-  const usersCollection = firestore().collection('Inventory').doc(firebase.auth().currentUser.uid).collection('minime');
-  const addBackground = firestore().collection('miniroom').doc(firebase.auth().currentUser.uid).collection('room').doc(firebase.auth().currentUser.uid);
-  const {isMinime,setisMinime,BuyItem,setcountItem} = useStore();
+  const usersCollection = firestore()
+    .collection('Inventory')
+    .doc(firebase.auth().currentUser.uid)
+    .collection('minime');
+  const addBackground = firestore()
+    .collection('miniroom')
+    .doc(firebase.auth().currentUser.uid)
+    .collection('room')
+    .doc(firebase.auth().currentUser.uid);
+  const {isMinime, setisMinime, BuyItem, setcountItem} = useStore();
   const [tool, setTool] = useState();
-   
+  const navigation = useNavigation();
+
   const getShopData = async () => {
     try {
       const data = await usersCollection.get();
-      setTool(data._docs.map(doc => ({ ...doc.data(), id: doc.id })));
+      setTool(data._docs.map(doc => ({...doc.data(), id: doc.id})));
     } catch (error) {
       console.log(error.message);
     }
   };
-  const updateMinime = (newaddress,newname,size) => {
-    addBackground.collection('minime').doc(firebase.auth().currentUser.uid+ 'mid').update({address:newaddress, name:newname,size:size});
+  const updateMinime = (newaddress, newname, size) => {
+    addBackground
+      .collection('minime')
+      .doc(firebase.auth().currentUser.uid + 'mid')
+      .update({address: newaddress, name: newname, size: size});
     setisMinime(newaddress);
     setcountItem();
   };
@@ -30,27 +51,34 @@ const MinimiInven = () => {
 
   return (
     <ScrollView>
-    <View style={styles.container}>
-      {
-        tool?.map((row, idx) => {
+      <View style={styles.container}>
+        {tool?.map((row, idx) => {
           {
-            return  <TouchableOpacity onPress={()=>{updateMinime(row.address,row.name,row.size)}} >
-            <Image source ={{uri:row.address}} style={{width:70,height:70,}} resizeMode="contain" resizeMethod = 'resize'></Image>
-            </TouchableOpacity>;} 
-      })
-      }
-    </View>
-    
+            return (
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate('CheckItem', {tool, idx});
+                  //updateMinime(row.address, row.name, row.size);
+                }}>
+                <Image
+                  source={{uri: row.address}}
+                  style={{width: 70, height: 70}}
+                  resizeMode="contain"
+                  resizeMethod="resize"></Image>
+              </TouchableOpacity>
+            );
+          }
+        })}
+      </View>
     </ScrollView>
-  )
-}
+  );
+};
 export default MinimiInven;
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row', // 혹은 'column'
     padding: 20,
     alignItems: 'center',
-    flexWrap:"wrap",
-},
+    flexWrap: 'wrap',
+  },
 });
-
